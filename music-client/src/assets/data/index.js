0c48d18afb3e099f03b6d58862e72aa796c5bd62
无声 <ivestszheng@qq.com>
2021-05-03 23:35:38 +0800

    init

---
+++ music-client/src/assets/data/index.js
@@ -0,0 +1,35 @@
+/*
+ * @Descripttion: 歌手索引
+ * @Author: 无声<ivestszheng@qq.com>
+ * @Date: 2021-05-03 21:28:15
+ * @LastEditTime: 2021-05-03 23:22:12
+ */
+const index = [
+  {index:'A'},
+  {index:'B'},
+  {index:'C'},
+  {index:'D'},
+  {index:'E'},
+  {index:'F'},
+  {index:'G'},
+  {index:'H'},
+  {index:'I'},
+  {index:'J'},
+  {index:'K'},
+  {index:'L'},
+  {index:'M'},
+  {index:'N'},
+  {index:'O'},
+  {index:'P'},
+  {index:'Q'},
+  {index:'R'},
+  {index:'S'},
+  {index:'T'},
+  {index:'U'},
+  {index:'V'},
+  {index:'W'},
+  {index:'X'},
+  {index:'Y'},
+  {index:'Z'}
+]
+export {index}
